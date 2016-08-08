16858cde27bc402f240e4de766b4e40a6d18e16a
Sagnik Bhattacharya bt me <sagnikb@ccpc197.cc.iitk.ac.in>
2016-08-09 01:09:02 +0530

    Bucket sort implemented

--- prog.c
+++ prog.c
@@ -40,18 +40,36 @@ int isEmpty(queue Q){
     }
 }
 
-void insert(int x, queue * Q){
+void enqueue(int x, queue * Q){
 	if(isEmpty(*Q)){    
 		node newNode = makeNode(x); 
 		Q->front = newNode;
                 Q->rear = newNode;
+		Q->itemN++;
      }
      else{
 	  node newNode = makeNode(x);
           Q->rear->next = newNode;
           Q->rear = newNode;
+	  Q->itemN++;
      }
 }
+
+void dequeue(queue * Q){
+	if(isEmpty(*Q)){
+		printf("The list is empty\n");
+		return;
+	}
+	else{
+		printf("%d\n",Q->front->data);
+		node temp = (Q->front);
+		Q->front = Q->front->next;
+		free(temp);
+		Q->itemN--;
+	}	
+}
+
+
 
 void printList(node head){
                node current = head;
@@ -62,11 +80,28 @@ void printList(node head){
 }
 
 int main(){
-    queue * Q = makeQueue();
-    insert(2,Q);
-    insert(5,Q);
-    insert(7,Q);
-    printList(Q->front);    
-    //system("PAUSE");
-    return 0;
+	int m, n;
+	printf("Please enter the maximum integer:\t");
+	scanf("%d", &m);
+	printf("Please enter the number of integers:\t");
+	scanf("%d", &n);
+	queue ** store = (queue**)malloc(m*sizeof(queue*));	
+	int i;
+	for(i = 0; i < m; i++){
+		store[i] = makeQueue();
+	}
+	int num;
+	for(i = 0; i < n; i++){
+		scanf("%d", &num);
+		enqueue(num, store[num]);
+	}
+	printf("\n\n\n");
+    	for(i = 0; i < n; i++){
+		while(!isEmpty(*store[i])){
+			dequeue(store[i]);
+		}
+	}
+    	//printList(Q->front);
+    	//system("PAUSE");
+    	return 0;
 }
