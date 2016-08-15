54e39559b867f65869e15eba998973148e4ea3b6
Sagnik <sagnik6696@gmail.com>
2016-08-16 03:30:11 +0530

    Heap sort code written, infinite loop on reduceKey

--- Python/heapSort.py
+++ Python/heapSort.py
@@ -11,28 +11,45 @@ class heap:
 		x = self.items[1]
 		y = self.items[self.num]
 		self.items[1] = -1
+		self.num = self.num - 1;
 		self.increaseKey(1,y)
 		return x
 	def insert(self,x):
 		self.num += 1
 		self.items.append(9999)
-		reduceKey(self.num, x)		
+		self.reduceKey(self.num, x)		
 	def reduceKey(self, i, x):
-		c = i
-		while c >= 1:
-
+		while (i >= 1):
+			p = i/2
+			if(self.items[p]>self.items[i]):
+				swap(i, p)
+				i = p
 	def increaseKey(self, i, x):
-		pass
+		while (2*i <= self.num):
+			if(2*i+1 <= self.num):
+				if((self.items[i] > self.items[2*i])or(self.items[i] > self.items[2*i+1])):
+					if(self.items[2*i]<self.items[2*i+1]):
+						swap(i,2*i)
+						i = 2*i
+					else:
+						swap(i,2*i+1)
+						i = 2*i+1
+			else:
+				if(self.items[i] > self.items[2*i]):
+					swap(i, 2*i)
+					i = 2*i	
 	def swap(self, i, j):
 		temp = self.items[i]
 		self.items[i] = self.items[j]
 		self.items[j] = temp
 	
-
-inputArray = []
+heap1 = heap()
 
 f = open('numbers.txt','r')
 for line in f:
 	number = int(line)
-	inputArray.append(number)
+	#inputArray.append(number)
+	heap1.insert(number)
 
+while(heap.num != 1):
+	print(heap1.deleteMin())
