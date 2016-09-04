d750187f86d0f9795784e1d4e9e446208e1e289e
Sagnik <sagnik6696@gmail.com>
2016-09-05 01:18:45 +0530

    Heapsort finally working! Yay!!

--- C/heapsort.c
+++ C/heapsort.c
@@ -2,7 +2,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#define size 5
+#define size 1000000
 
 struct heap{
 	int arr[size+1];
@@ -19,6 +19,10 @@ void swap(int id1, int id2, int arr[]){
 }
 
 void reduceKey(int id, int x, heap * h){
+	if(h->n == 1){
+	       	h->arr[1] = x;
+		return;
+	}
 	int q = id;
 	int p = id/2;
 	h->arr[id] = x;
@@ -35,28 +39,29 @@ void reduceKey(int id, int x, heap * h){
 }
 
 void increaseKey(int id, int x, heap * h){
-	h->arr[id] = x;
-	int len = h->n;
+	h->arr[id]=x;
+	int len=h->n;
 	while(2*id <= len){
-		if(2*id + 1 > len){
-			if(h->arr[id] > h->arr[2*id]){
-				swap(id, 2*id, h->arr);
+		if(2*id+1 <= len){
+			if((h->arr[2*id] >= h->arr[id])&&(h->arr[2*id+1] >= h->arr[id])){
+			       break;
+			}
+			else if(h->arr[2*id] <= h->arr[2*id+1]){
+				swap(2*id, id, h->arr);
 				id = 2*id;
+			}
+			else{
+				swap(2*id+1, id, h->arr);
+				id = 2*id + 1;
 			}
 		}
 		else{
-			if((h->arr[id] < h->arr[2*id])&&(h->arr[id] < h->arr[2*id+1])){
+			if((h->arr[2*id]>=h->arr[id])){
 				break;
 			}
 			else{
-				if((h->arr[2*id] < h->arr[2*id])){
-					swap(2*id, id, h->arr);
-					id = 2*id;
-				}
-				else{
-					swap(2*id + 1, id, h->arr);
-					id = 2*id + 1;
-				}
+				swap(2*id, id, h->arr);
+				id = 2*id;
 			}
 		}
 	}
@@ -82,19 +87,29 @@ void insert(int x, heap * h){
 	reduceKey(h->n, x, h);
 }
 
+void print(heap * h){
+	for(int i = 1; i <= h->n; i++){
+		printf("%d\n", h->arr[i]);
+	}
+	printf("-------------\n");
+}
 
 int main(){
 	heap * h = (heap *)malloc(sizeof(heap));
-	h->n = 1;
+	h->n = 0;
+	h->arr[0] = 0;
+	for(int i = 1; i <= size; i++){
+		h->arr[i] = -1;
+	}
 	FILE * fp = fopen("number.txt", "r");
 	int num;
 	for(int i = 1; i <= size; i++){
-		scanf("%d", &num);
+		fscanf(fp,"%d", &num);
 		insert(num, h);
 	}
-	printf("**%d\n", findmin(h));
 	for(int i = 1; i <= size; i++){
-		printf("%d\n", h->arr[i]);
+		printf("%d\n", delMin(h));
 	}
+	free(h);
 	return 0;
 }
