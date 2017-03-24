23622099c5fcccfd30d83cfc66f9e961344a443b
sagnikb <sagnik6696@gmail.com>
2017-03-25 01:06:44 +0530

    numbers.py

---
+++ Python/numbers.py
@@ -0,0 +1,14 @@
+import numpy as np
+#np.show_config()
+#print(np.__version__)
+Z = np.zeros(10)
+print(Z)
+#np.info(np.add)
+Z[4] = 5
+Z = np.arange(1, 50, 2)
+print(Z)
+Z = Z[::-7]
+print(Z)
+Z = np.arange(0,9).reshape(3,3)
+print(Z)
+
