68918f0a3581df86846b82e88992db14e0fef0b7
Sagnik <sagnikb>
2017-03-25 23:40:09 +0530

    100 numpy exercises

--- Python/numpy/num.py
+++ Python/numpy/num.py
@@ -1,14 +1,29 @@
 import numpy as np
 #np.show_config()
 #print(np.__version__)
-Z = np.zeros(10)
-print(Z)
+#Z = np.zeros(10)
+#print(Z)
 #np.info(np.add)
-Z[4] = 5
-Z = np.arange(1, 50, 2)
-print(Z)
-Z = Z[::-7]
-print(Z)
-Z = np.arange(0,9).reshape(3,3)
-print(Z)
+#Z[4] = 5
+#Z = np.arange(1, 50, 2)
+#print(Z)
+#Z = Z[::-7]
+#print(Z)
+#Z = np.arange(0,9).reshape(3,3)
+#print(Z)
+#nz = np.nonzero([1,2,0,0,4,0])
+#print(nz)
+#Z = np.eye(3)
+#print(Z)
+#Z = np.random.random((3,3,3))
+#print(Z)
+#Z = np.random.random((10,10))
+#Zmin, Zmax = Z.min(), Z.max()
+#print(Zmin, Zmax)
+#Z = np.random.random(30)
+#m = Z.mean()
+#print(m)
+#Z = np.ones((10,10))
+#Z[1:-1,1:-1] = 0
+#print(Z)
 
